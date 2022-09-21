1735b4619933b92b0500f1779936f937b1e86bca
surbhi <84731799+surbhigulhana@users.noreply.github.com>
2022-09-21 12:53:22 +0530

    Update dbConnection.js

--- config/dbConnection.js
+++ config/dbConnection.js
@@ -1,7 +1,7 @@
   const mongoose = require("mongoose");
 mongoose
   .connect(
-    "mongodb+srv://surbhi:surbhi@123@cluster0.cqpb1y1.mongodb.net/?retryWrites=true&w=majority",
+    "mongodb+srv://surbhi:surbhi@123@cluster0.cqpb1y1.mongodb.net/Lash?retryWrites=true&w=majority",
     {
       useNewUrlParser: true,
       useUnifiedTopology: true,
